7ff559d16eefc423debb0df47164f3ef9163ffa1
Be4nz <benas.andriuscenka@gmail.com>
2024-11-10 22:26:19 +0200

    invite-nav

--- src/components/navigation/NavigationBar.jsx
+++ src/components/navigation/NavigationBar.jsx
@@ -14,6 +14,9 @@ function NavigationBar() {
 				<Button onClick={() => navigate('/run-list')} sx={{ my: 2, color: 'white', display: 'block' }}>
 					Bėgimai
 				</Button>
+				<Button onClick={() => navigate('/invite-list')} sx={{ my: 2, color: 'white', display: 'block' }}>
+					Pakvietimai
+				</Button>
 				{showAll && (
 					<>
 						<Button onClick={() => navigate('/run/1')} sx={{ my: 2, color: 'white', display: 'block' }}>
---
+++ src/pages/InviteForm.jsx
@@ -0,0 +1,81 @@
+import React, { useState } from 'react';
+import { Box, Typography, Card, CardActionArea, CardContent, TextField, Button } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+
+function InviteForm() {
+	const navigate = useNavigate();
+	const [inviteText, setInviteText] = useState('');
+
+	const handleCardClick = () => {
+		navigate('/run/1');
+	};
+
+	const handleInputChange = (event) => {
+		setInviteText(event.target.value);
+	};
+
+	const handleSubmit = () => {
+		console.log('Invite text submitted:', inviteText);
+		navigate('/sent-invite-list');
+	};
+
+	return (
+		<Box display='flex' flexDirection='column' alignItems='center' justifyContent='center' height='100vh' padding={3}>
+			<Card sx={{ maxWidth: 300, borderRadius: 2, boxShadow: 3, mb: 2 }}>
+				<CardActionArea onClick={handleCardClick}>
+					<CardContent>
+						<Typography
+							variant='h5'
+							component='div'
+							sx={{
+								textAlign: 'center',
+								fontWeight: 'bold',
+							}}
+						>
+							Grįžti į renginį
+						</Typography>
+					</CardContent>
+				</CardActionArea>
+			</Card>
+
+			{/* Invite creation form */}
+			<Box
+				component='form'
+				onSubmit={(e) => {
+					e.preventDefault();
+					handleSubmit();
+				}}
+				sx={{ width: '100%', maxWidth: 300 }}
+			>
+				<Typography variant='h6' textAlign='center' mb={2}>
+					Pakvietimas
+				</Typography>
+				<TextField
+					label='Kviečiamo vartotojo el. paštas'
+					variant='outlined'
+					fullWidth
+					multiline
+					rows={1}
+					value={inviteText}
+					onChange={handleInputChange}
+					sx={{ mb: 2 }}
+				/>
+				<TextField
+					label='Pakveitimo tekstas'
+					variant='outlined'
+					fullWidth
+					multiline
+					rows={4}
+					value={inviteText}
+					onChange={handleInputChange}
+					sx={{ mb: 2 }}
+				/>
+				<Button variant='contained' color='primary' fullWidth onClick={handleSubmit}>
+					Siūsti pakvietimą
+				</Button>
+			</Box>
+		</Box>
+	);
+}
+
+export default InviteForm;
