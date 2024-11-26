fa80d02ee7591600ad8476fbde9cb2c7962abadf
Vilius Tamašauskas <viliustam36@gmail.com>
2024-11-26 19:20:13 +0200

    Showing multiple events

--- src/api/eventAPI.js
+++ src/api/eventAPI.js
@@ -4,6 +4,26 @@ const axiosInstance = axios.create({
     baseURL: 'http://localhost:5000',
 });
 
+export const fetchPublicEvents = async () => {
+    try {
+        const response = await axiosInstance.get('/event/events/public');
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching public events:', error.response?.data || error.message);
+        throw error;
+    }
+};
+
+export const fetchPrivateEvents = async () => {
+    try {
+        const response = await axiosInstance.get('/event/events/private');
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching public events:', error.response?.data || error.message);
+        throw error;
+    }
+};
+
 export const postEvent = async (eventData) => {
     try {
         const response = await axiosInstance.post('/event/event/postevent', eventData);
